47b100c2360bd73400d48fe8f7b8a51d49d8e60e
shiba <rp.shiba2@gmail.com>
2021-08-30 19:49:13 +0530

    like done

--- src/components/Post/Like.js
+++ src/components/Post/Like.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { database } from '../../firebase';
 import { makeStyles } from '@material-ui/core/styles';
-// import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import "../Styles/Like.css"
 import FavoriteIcon from '@material-ui/icons/Favorite';
 const useStyles = makeStyles({
@@ -12,10 +11,6 @@ const useStyles = makeStyles({
         fontSize: '32px',
     },
     icon: {
-        // backgroundColor: "red"
-        // position: "absolute",
-        // bottom: "-5vh",
-        // fontSize: "2rem",
         color: '#3f3f41',
         cursor: 'pointer',
         fontSize: '32px',
@@ -35,7 +30,7 @@ const useStyles = makeStyles({
 function Like({ userData , postData , currentUserObj }) {
     // console.log(userData);
     // console.log(postData);
-    console.log(currentUserObj);
+    // console.log(currentUserObj);
     const classes = useStyles();
     const [isLiked, setLiked] = useState(false);
     const [CurrentUserObj,setCurrentUserObj]=useState("");
@@ -46,43 +41,28 @@ function Like({ userData , postData , currentUserObj }) {
        let postRef=await database.posts.doc(postid).get();
        let post=postRef.data();
        console.log(post);
-       let likes=post.Likes;
-       console.log(likes);
- 
-       if(isLiked==false){
-           database.posts.doc(postid).update({
-               "likes":[...likes,currentUserObj.Uid]
-           })
-       }
-       else{
-           let likes=post.likes.filter(lkuid=>{
-               return lkuid !=currentUserObj.Uid;
-           })
-           database.posts.doc(postid).update({
-               "likes":likes
-           })
-       }
+  
+           if(isLiked==true){
+               let newLikeArr=post.Likes.filter(el=>{
+                   return el !=currentUserObj.Uid;
+               })
+               await database.posts.doc(postid).update({
+                   Likes:newLikeArr
+               })
+           }
+           else{
+               let newLikeArr=[...post.Likes,currentUserObj.Uid];
+               await database.posts.doc(postid).update({
+                   Likes:newLikeArr
+               })
+           }
        setLiked(!isLiked);
 
    }
-    // useEffect(() => {
-    //     let check = postData?.Likes.includes(userData?.Uid) ? true : false;
-    //     setLiked(check);
-    // }, [])
+  
 
     return (
         <div className='like' style={{ display:'flex', justifyContent:'flex-start'}}>
-            {/* {
-                like != null
-                    ? <>{like == false
-                        ? <><FavoriteBorderIcon className={classes.unlike} onClick={handleLike} /> </>
-                        : <><FavoriteIcon className={classes.like} onClick={handleLike} /></>
-                    }</>
-                    : <></>
-            } */}
-             {/* <FavoriteBorderIcon className={[classes.icon, classes.heart, isLiked == false ? classes.notSelected : classes.selected]}
-                                onClick={() => { handleLike(postData.PostId) }}
-                            ></FavoriteBorderIcon> */}
           <FavoriteIcon
           className={[classes.icon,classes.heart  ,isLiked==false? classes.notSelected  : classes.selected ]}
           onClick={()=>{handleLike(postData.PostId)}}
